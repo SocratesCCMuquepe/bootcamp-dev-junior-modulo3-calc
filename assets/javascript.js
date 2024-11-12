e9706c18a58c444af4c90ad195e76bc0d8c3e037
Socrates Muquepe <socycrates@hotmail.com>
2024-11-12 21:35:39 +0100

    closes #4 - Implementar as 4 operações matemáticas

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,9 +1,14 @@
 
 
-function calc(){
+function calc(operacao) {
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
-    var result = num1 + num2;
+    var result = 0;
+    switch (operacao) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+    }
     document.getElementById("result").value = result;
-
 }
